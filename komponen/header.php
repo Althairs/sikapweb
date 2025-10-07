b0d6bda6e6ff654bc860a0bd5df8cd7d73f39018
Althairs <148575542+Althairs@users.noreply.github.com>
2025-10-07 15:23:29 +0800

    Add files via upload

---
+++ komponen/header.php
@@ -0,0 +1,13 @@
+<!-- Header Utama -->
+<div class="main-header">
+  <div class="title">
+    <span class="mobile-hidden">SIKAP - SISTEM INFORMASI KASUS ANAK DAN PEREMPUAN</span>
+    <span class="mobile-visible">SIKAP</span>
+  </div>
+  <div class="user-info">
+    <span class="me-2 mobile-hidden">Admin</span>
+    <span class="me-2 mobile-visible">A</span>
+    <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" 
+         width="35" class="rounded-circle border">
+  </div>
+</div>
--- komponen/sidebar.php
+++ komponen/sidebar.php
@@ -1,1 +1,34 @@
-
+<!-- Sidebar -->
+<div class="sidebar">
+  <div class="logo">
+    <img src="logosikap.png" alt="Logo SIKAP">
+    <h5>BIDANG PPA</h5>
+    <h6>DPPKBP3A</h6>
+  </div>
+  <div class="menu">
+    <a href="dashboard.php"><i class="bi bi-house-fill"></i> Dashboard</a>
+    <div class="dropdown-wrapper">
+      <a href="#" class="dropdown-hover d-flex align-items-center">
+        <i class="bi bi-folder-plus"></i> Input Kasus
+        <i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <div class="submenu ms-3">
+        <a href="KTA.php" class="d-block py-1"><i class="bi bi-person-fill"></i> KTA</a>
+        <a href="KTP.php" class="d-block py-1"><i class="bi bi-gender-female"></i> KTP</a>
+      </div>
+    </div>
+    <div class="dropdown-wrapper">
+      <a href="#" class="dropdown-hover d-flex align-items-center">
+        <i class="bi bi-clipboard-data"></i> Laporan
+        <i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <div class="submenu ms-3">
+        <a href="rekapanKTA.php" class="d-block py-1"><i class="bi bi-person-fill"></i> Rekapan KTA</a>
+        <a href="rekapanKTP.php" class="d-block py-1"><i class="bi bi-gender-female"></i> Rekapan KTP</a>
+      </div>
+    </div>
+  </div>
+  <div class="logout-btn">
+    <a href="login.html"><i class="bi bi-box-arrow-right"></i> Logout</a>
+  </div>
+</div>
